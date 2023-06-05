4222d7c3078ee2cd852197387cf88137b740569f
HOLLÝ Andrej <432569@slsp.sk>
2023-06-05 15:58:38 +0200

    eventy

--- models/core/event_l10.sql
+++ models/core/event_l10.sql
@@ -6,11 +6,15 @@
 }}
 WITH src_event AS (
     select * from {{ref("src_event")}}
-)
+),
+random_user as (select user_id from KEBOOLA_10.WORKSPACE_5901.USER_L10 order by random() limit 1)
 select UUID_STRING() as event_id,
       "SOURCE_SYSTEM",
-      "EVENT_DATE"
+      "EVENT_DATE",
+      u.user_id as user_id
   from src_event
+  join random_user u
+    on 1=1
  where "SOURCE_SYSTEM" is not null
  {% if is_incremental() %}
    and "EVENT_DATE" >= (select max("EVENT_DATE") from {{ this }})
